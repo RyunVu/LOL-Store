6b561251316049bb06426a9d75e0b2fd248edd52
Ryun KuroShi <ryun3980@gmail.com>
2025-12-31 12:01:18 +0700

    git add .gitignore git commit -m "chore: remove build artifacts and ignore bin/obj"

--- backend/LoLStore.Data/obj/Debug/net9.0/LoLStore.Data.AssemblyInfo.cs
+++ backend/LoLStore.Data/obj/Debug/net9.0/LoLStore.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoLStore.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+069ac4075dd0e07cc7df41c51380c12868c40cd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e49dcc895b7a815fcc0c6c3ecdd3c7614afe8f1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoLStore.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoLStore.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
